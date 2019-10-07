2d25f6ba51d74b5ed8f161f3ad610126bafa2507
Tom <tomtarpeydev@gmail.com>
2019-10-07 10:42:24 +0100

    Added challenge

---
+++ src/challenge.py
@@ -0,0 +1,19 @@
+
+ # chalenge. What is the complexity of this algorithm?
+
+def complex_algo(items):
+
+    for _ in range(5):
+        print ("Python is awesome")
+
+    for item in items:
+        print(item)
+
+    for item in items:
+        print(item)
+
+    print("Big O")
+    print("Big O")
+    print("Big O")
+
+complex_algo([4, 5, 6, 8])

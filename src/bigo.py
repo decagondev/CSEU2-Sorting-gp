5b1979fd7d6b51eb95b29d88acd12392d3b07a6d
Tom <tomtarpeydev@gmail.com>
2019-10-07 08:40:24 +0100

    fixed some linting

--- src/bigo.py
+++ src/bigo.py
@@ -14,21 +14,21 @@ linear_algo([4, 5, 6, 8])
 
 # What is the big O of this ??
 
-def linear_algo(items):
+def linear_algo_2(items):
     for item in items:
         print(item)
 
     for item in items:
         print(item)
 
-linear_algo([4, 5, 6, 8])
+linear_algo_2([4, 5, 6, 8])
 
 
 # O(n^2)
 def quadratic_algo(items):
     for item in items:
         for item2 in items:
-            print(item, ' ' ,item)
+            print(item, ' ' ,item2)
 
 quadratic_algo([4, 5, 6, 8])
 
@@ -36,7 +36,7 @@ quadratic_algo([4, 5, 6, 8])
 
 def complex_algo(items):
 
-    for i in range(5):
+    for _ in range(5):
         print ("Python is awesome")
 
     for item in items:
